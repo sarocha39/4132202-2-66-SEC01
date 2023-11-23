3c64ac80b0cf5edde38a5c6ef445469a51a545bc
sarocha39 <650112418039@bru.ac.th>
2023-11-23 16:28:52 +0700

    LAB3

---
+++ LAB3/html/condb.php
@@ -0,0 +1,23 @@
+<?php
+$host = "db";
+$username = "root";
+$password = "1234";
+$db = "internet";
+
+$conn = mysqli_connect($host,$username,$password,$db);
+mysqli_query($conn,"SET NAME utf8");
+
+try{
+    $conn = mysqli_connect($host,$username,$password,$db);
+    mysqli_query($conn,"SET NAME utf8");
+}
+catch(Exception $e){
+    $error = $e->getMessage();
+    error_log($error);
+    echo $error;
+
+}
+
+
+
+?>
